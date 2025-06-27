67f5ea21f98baf73f473ff18cfc1fd0c421e8376
SWARNIM <swarnimchandve23@gmail.com>
2025-06-27 14:15:54 +0530

    Screen3

--- src/app/data.ts
+++ src/app/data.ts
@@ -21,38 +21,38 @@
 
 
 
-export const inputs2 = [
-  { label: "Location ID ", placeholder: "CLID/01", type: "text", required : "true"  },
-  { label: "Location Registration Date ", placeholder: "dd-mm-yyyy", type: "date" ,required : "true" },
-  { label: "Location Display Name ", placeholder: "Enter Legal Entity Name", type: "text" ,required : "true" },
-  { label: "Status", placeholder: "Select Status", type: "text" , logo:"logo"},
-  { label: "Country", placeholder: "Select Country (Default India)", type: "text", dropdown: true },
-  { label: "State/Province", placeholder: "Select State", type: "text",dropdown: true  },
-  { label: "PIN Code", placeholder: "6-digit Code", type: "number" },
-  { label: "City/District", placeholder: "Enter City/District Name", type: "text", },
-  { label: "Address Line 1", placeholder: "Enter Address Line 1", type: "text" },
-  { label: "Address Line 2", placeholder: "Enter Address Line 2", type: "text" },
-  { label: "Address Line 3", placeholder: "Enter Address Line 3", type: "text", },
-  { label: "Address Line 4", placeholder: "Enter Address Line 4", type: "text" },
-  
-];
-
-
-
-
-
-
-
-// export const inputs3 = [
-//   { label: "Location Display Name ", placeholder: "Plant 1,Thane,400601, MH", type: "text" ,required : "true" },
-//   { label: "GSTIN", placeholder: "27AAGCD4662E1ZP", type: "date", required : "true" },
-//   { label: "Status ", placeholder: "Active", type: "text",required : "true"  },
-//   { label: "PF Act Applicability ", placeholder: "DD/Yes,No", type: "text",dropdown: true , required : "true"  },
-//   { label: "PF Registration No. ", placeholder: "Smart Field", type: "text", required : "true"  },
-//   { label: "ESI Act Applicability ", placeholder: "DD/Yes,No", type: "text" ,dropdown: true, required : "true"  },
-//   { label: "ESI Registration No. ", placeholder: "Smart Field", type: "text",required : "true"  },
-//   { label: "PT Registration No.", placeholder: "Smart Field", type: "text"},
-//   { label: "LWF Act Applicability", placeholder: "DD/Yes,No", type: "text",dropdown: true  },
-//   { label: "LWF Registration No.", placeholder: "Smart Field", type: "date" },
+// export const inputs2 = [
+//   { label: "Location ID ", placeholder: "CLID/01", type: "text", required : "true"  },
+//   { label: "Location Registration Date ", placeholder: "dd-mm-yyyy", type: "date" ,required : "true" },
+//   { label: "Location Display Name ", placeholder: "Enter Legal Entity Name", type: "text" ,required : "true" },
+//   { label: "Status", placeholder: "Select Status", type: "text" , logo:"logo"},
+//   { label: "Country", placeholder: "Select Country (Default India)", type: "text", dropdown: true },
+//   { label: "State/Province", placeholder: "Select State", type: "text",dropdown: true  },
+//   { label: "PIN Code", placeholder: "6-digit Code", type: "number" },
+//   { label: "City/District", placeholder: "Enter City/District Name", type: "text", },
+//   { label: "Address Line 1", placeholder: "Enter Address Line 1", type: "text" },
+//   { label: "Address Line 2", placeholder: "Enter Address Line 2", type: "text" },
+//   { label: "Address Line 3", placeholder: "Enter Address Line 3", type: "text", },
+//   { label: "Address Line 4", placeholder: "Enter Address Line 4", type: "text" },
   
 // ];
+
+
+
+
+
+
+
+export const inputs3 = [
+  { label: "Location Display Name ", placeholder: "Plant 1,Thane,400601, MH", type: "text" ,required : "true" },
+  { label: "GSTIN", placeholder: "27AAGCD4662E1ZP", type: "date", required : "true" },
+  { label: "Status ", placeholder: "Active", type: "text",required : "true"  },
+  { label: "PF Act Applicability ", placeholder: "DD/Yes,No", type: "text",dropdown: true , required : "true"  },
+  { label: "PF Registration No. ", placeholder: "Smart Field", type: "text", required : "true"  },
+  { label: "ESI Act Applicability ", placeholder: "DD/Yes,No", type: "text" ,dropdown: true, required : "true"  },
+  { label: "ESI Registration No. ", placeholder: "Smart Field", type: "text",required : "true"  },
+  { label: "PT Registration No.", placeholder: "Smart Field", type: "text"},
+  { label: "LWF Act Applicability", placeholder: "DD/Yes,No", type: "text",dropdown: true  },
+  { label: "LWF Registration No.", placeholder: "Smart Field", type: "date" },
+  
+];
